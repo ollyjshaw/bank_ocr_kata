cce0d234192eba9739da42c5da6ddecbc4fea85b
Oliver Shaw <oliver.shaw@sage.com>
2014-04-04 11:00:23 +0100

    All numbers recognised

--- src/main/java/bank_ocr_kata/Split.java
+++ src/main/java/bank_ocr_kata/Split.java
@@ -5,15 +5,46 @@ import java.util.List;
 
 public class Split {
 
-    private static final String ZERO = " _ " +
+    private static final String ZERO =
+            " _ " +
             "| |" +
             "|_|";
-    private static final String ONE = "   " +
+    private static final String ONE =
+            "   " +
             "  |" +
             "  |";
+    private static final String TWO =
+            " _ " +
+            " _|" +
+            "|_  ";
+    private static final String THREE =
+            " _ " +
+            " _|" +
+            " _|";
     private static final String FOUR = "   " +
             "|_|" +
             "  |";
+    private static final String FIVE =
+            " _ " +
+            "|_ " +
+            " _|";
+    private static final String SIX =
+            " _ " +
+            "|_ " +
+            "|_|";
+    private static final String SEVEN =
+            " _ " +
+            "  |" +
+            "  |";
+    private static final String EIGHT =
+            " _ " +
+            "|_|" +
+            "|_|";
+    private static final String NINE =
+            " _ " +
+            "|_|" +
+            " _|";
+
     List<Character> chars = new LinkedList<Character>();
 
     @Override
@@ -34,8 +65,14 @@ public class Split {
     public int toInt() throws NotANumberException {
         if (toString().equals(ZERO)) return 0;
         if (toString().equals(ONE)) return 1;
+        if (toString().equals(TWO)) return 2;
+        if (toString().equals(THREE)) return 3;
         if (toString().equals(FOUR)) return 4;
+        if (toString().equals(FIVE)) return 5;
+        if (toString().equals(SIX)) return 6;
+        if (toString().equals(SEVEN)) return 7;
+        if (toString().equals(EIGHT)) return 8;
+        if (toString().equals(NINE)) return 9;
         throw new NotANumberException();
-        //To change body of created methods use File | Settings | File Templates.
     }
 }
--- src/test/java/bank_ocr_kata/TestSplit.java
+++ src/test/java/bank_ocr_kata/TestSplit.java
@@ -34,23 +34,78 @@ public class TestSplit {
     @Test
     public void testTwo() throws NotANumberException {
         Split s = new Split();
-        s.addString("   |_|  |");
-        assertEquals(4, s.toInt());
+        s.addString(" _ " +
+                    " _|" +
+                    "|_  ");
+        assertEquals(2, s.toInt());
     }
 
     @Test
     public void testThree() throws NotANumberException {
         Split s = new Split();
-        s.addString("   |_|  |");
-        assertEquals(4, s.toInt());
+        s.addString(" _ " +
+                    " _|" +
+                    " _|");
+        assertEquals(3, s.toInt());
     }
 
     @Test
     public void testFour() throws NotANumberException {
         Split s = new Split();
-        s.addString(    "   "+
-                "|_|"+
+        s.addString(
+                "   " +
+                "|_|" +
                 "  |");
         assertEquals(4, s.toInt());
+    }
+
+    @Test
+    public void testFive() throws NotANumberException {
+        Split s = new Split();
+        s.addString(
+                " _ " +
+                "|_ " +
+                " _|");
+        assertEquals(5, s.toInt());
+    }
+
+    @Test
+    public void testSix() throws NotANumberException {
+        Split s = new Split();
+        s.addString(
+                        " _ " +
+                        "|_ " +
+                        "|_|");
+        assertEquals(6, s.toInt());
+    }
+
+    @Test
+    public void testSeven() throws NotANumberException {
+        Split s = new Split();
+        s.addString(
+                        " _ " +
+                        "  |" +
+                        "  |");
+        assertEquals(7, s.toInt());
+    }
+
+    @Test
+    public void testEight() throws NotANumberException {
+        Split s = new Split();
+        s.addString(
+                        " _ " +
+                        "|_|" +
+                        "|_|");
+        assertEquals(8, s.toInt());
+    }
+
+    @Test
+    public void testNine() throws NotANumberException {
+        Split s = new Split();
+        s.addString(
+                        " _ " +
+                        "|_|" +
+                        " _|");
+        assertEquals(9, s.toInt());
     }
 }
